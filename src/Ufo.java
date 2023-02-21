700b785b00ceb17ffdcb86513eab479aab242022
leo.tautz <slyther2839@gmail.com>
2023-02-21 09:44:17 +0100

    .

--- src/Ufo.java
+++ src/Ufo.java
@@ -1,38 +1,19 @@
 import GLOOP.*;
 public class Ufo{
-    private GLTorus rumpf; 
-    private GLKugel cockpit;
-    private GLKegel fluegel1, fluegel2;
-    //[...]
+    private GLKugel UFO;
+
 
     public Ufo(){
-        rumpf = new GLTorus(0,0,0,50,20);
-        rumpf.skaliere (0.5,1,0.5);
-        rumpf.setzeQualitaet(24);
+        UFO = new GLKugel(0,0,0,50);
 
-        cockpit = new GLKugel(0,0,0,25);
-        cockpit.setzeMaterial(GLMaterial.GOLD);
-
-        fluegel1 = new GLKegel(-60,-10,0,15,70);
-        fluegel1.skaliere(0.2,1,1);        
-        fluegel1.drehe(0,90,0, -60,10,0);
-        fluegel1.drehe(0,0,45, 0,0,0);
-        fluegel1.setzeFarbe(0,0,1);
-
-        fluegel2 = new GLKegel(60,-10,0,15,70);
-        fluegel2.skaliere(0.2,1,1);        
-        fluegel2.drehe(0,-90,0, 60,10,0);
-        fluegel2.drehe(0,0,-45, 0,0,0);
-        fluegel2.setzeFarbe(0,0,1);  
-        //[...]
     }
  
     public void bewegeLinks(){
-        //[...]
+        UFO.verschiebe(-1,0,0);
     }
 
-    public void bewegeRechts(){       
-        //[...]       
+    public void bewegeRechts(){
+        UFO.verschiebe(1,0,0);
     } 
 
     public void explodiere(){
--- src/Ufospiel.java
+++ src/Ufospiel.java
@@ -5,18 +5,18 @@ public class Ufospiel{
     private GLTastatur tastatur;
     private GLHimmel himmel;
 
-    private Ufo dasUfo;
+    private Ufo UFO;
     private Asteroid asteroid1,asteroid2,asteroid3;
     //[...]
 
     public Ufospiel(){
-        kamera = new GLSchwenkkamera(800,600);  
+        kamera = new GLEntwicklerkamera(800,600);
         kamera.verschiebe(0,200,500);
         licht  = new GLLicht();
         tastatur = new GLTastatur();
         himmel = new GLHimmel("src/img/Sterne.jpg");
 
-        dasUfo = new Ufo();
+        UFO = new Ufo();
 
         asteroid1 = new Asteroid();
         //[...]
@@ -27,10 +27,14 @@ public class Ufospiel{
     public void fuehreAus(){
         while(!tastatur.esc()){           
             asteroid1.bewegeDich();
-            //[...]
+            if (tastatur.links())
+                UFO.bewegeLinks();
+            if (tastatur.rechts())
+                UFO.bewegeRechts();
 
             Sys.warte();
         }
         Sys.beenden(); 
     }
+
 }
